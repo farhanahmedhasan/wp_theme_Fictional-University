3b8e74da4c56a8738c04f0892cf87bf282787557
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2023-11-04 23:24:14 +0600

    covert statice html to wp

--- functions.php
+++ functions.php
@@ -1,8 +1,11 @@
 <?php
 
 function university_files(){
-    //1st arg nickname for our main stylesheet 2nd arg file location
-    wp_enqueue_style('university_main_styles', get_stylesheet_uri()); //2nd arg here gets the wp main style file by calling another func
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
+    wp_enqueue_style('google-fonts', "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+    wp_enqueue_style('font-awesome', "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
+    wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
+    wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
 //This loads all the scripts and style files by the name of 2nd args which is a callback function
